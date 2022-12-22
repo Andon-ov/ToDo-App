d2019f71af9951bc5cceea0e3308737ccbecc8e9
BloodTaint <Bloodtaint@yahoo.com>
2022-12-22 14:49:25 +0200

    Django rest app

---
+++ todo_app/todo_app/api_todos/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from todo_app.api_todos.views import ListCreateTodoApiView, ListCategoriesApiView, DetailsTodoApiView
+
+urlpatterns = (
+    path('', ListCreateTodoApiView.as_view(), name='api list todo'),
+    path('<int:pk>/', DetailsTodoApiView.as_view(), name='api details todo'),
+    path('categories/', ListCategoriesApiView.as_view(), name='api list categories'),
+)
